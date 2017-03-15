6eab6c2a1436eb85242f75ae3dde1d6b945637d3
Mukesh B <mbhalani3@gmail.com>
2017-03-15 16:39:06 -0400

    readFile comp

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,8 +6,16 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, 'utf8', function(err, res) {
+    if (err) {
+      callback(err, null);
+    } else {
+      var firstLine = res.split('\n')[0];
+      callback(null, firstLine);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
